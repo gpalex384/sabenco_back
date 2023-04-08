0b4ba66e5f93af18a9abe7f7bc222feb698a2502
gpalex384 <agarc384@gmail.com>
2023-04-08 18:15:46 +0200

    get user and create event functions

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-import models
+import models, schemas
 from sqlalchemy.orm import Session, joinedload
 
 # Read all the events that belong to a given category whose start date is between two dates
@@ -6,3 +6,17 @@ def get_event_by_category(db: Session, startdate: str, enddate:str, category:str
     return db.query(models.Event).filter(models.Event.startdate >= startdate,
                                          models.Event.startdate <= enddate,
                                          models.EventCategory.category_id == category).all()
+
+# Create an event given the title, detail, start date and end date
+def create_event(db: Session, event: schemas.EventBase, user_id: str):
+    db_event = models.Event(**event.dict())
+    db_event.createdby = user_id
+    db_event.updatedby = user_id
+    db.add(db_event)
+    db.commit()
+    db.refresh(db_event)
+    return db_event
+
+# Get the user data given the userId
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
