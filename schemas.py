6dd39f729a7caec2210fbbce143a6bfe7c144f00
gpalex384 <agarc384@gmail.com>
2023-04-08 18:18:29 +0200

    Create User and Role schemas. Update event schemas

--- schemas.py
+++ schemas.py
@@ -5,6 +5,28 @@ from pydantic.schema import Any, Union
 from pydantic.utils import GetterDict
 from sqlalchemy import Date
 
+class UserBase(BaseModel):
+    password: str
+
+class User(UserBase):
+    id: str
+    username: Union[str,None]
+    usermail: str
+    role_id: str
+    
+    class Config:
+            orm_mode = True
+
+class Role(BaseModel):
+    id: str
+    name: str
+
+    users: List[User] = []
+
+    class Config:
+        orm_mode = True
+
+
 class EventCategoryGetter(GetterDict):
     def get(self, key: str, default: Any = None) -> Any:
         if key in {'id', 'name', 'description','categorytype_id','created','updated'}:
@@ -20,17 +42,21 @@ class EventCategory(BaseModel):
     created: date
     updated: date
     
-
     class Config:
         orm_mode = True
         getter_dict = EventCategoryGetter
 
-class Event(BaseModel):
-    id: str = Field(alias='id')
+class EventBase(BaseModel):
     title: str
     detail: Union[str,None]
     startdate: date
     enddate: Union[date,None]
+    
+    class Config:
+        orm_mode = True
+
+class Event(EventBase):
+    id: str = Field(alias='id')
     created: date
     updated: date
 
@@ -41,11 +67,12 @@ class Event(BaseModel):
 
 class CategoryEventGetter(GetterDict):
     def get(self, key: str, default: Any = None) -> Any:
-        if key in {'id', 'title', 'detail','startdate','enddate','created','updated'}:
+        if key in {'id', 'title', 'detail','startdate','enddate', 'created', 'updated'}:
             return getattr(self._obj.event, key)
         else:
-            return super(EventCategoryGetter, self).get(key, default)
-        
+            return super(CategoryEventGetter, self).get(key, default)
+
+
 class CategoryEvent(BaseModel):
     id: str = Field(alias='id')
     title: str
@@ -54,10 +81,11 @@ class CategoryEvent(BaseModel):
     enddate: Union[date,None]
     created: date
     updated: date
-
+    
     class Config:
-        orm_mode = True
         getter_dict = CategoryEventGetter
+        orm_mode = True
+    
 
 class Category(BaseModel):
     id: str = Field(alias='id')
@@ -67,7 +95,6 @@ class Category(BaseModel):
     created: date
     updated: date
     
-
     events: List[CategoryEvent]
     
     class Config:
