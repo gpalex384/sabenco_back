06a1fd87c04f5e2aaaf5fdca385274f6969003e8
gpalex384 <agarc384@gmail.com>
2023-04-08 18:17:53 +0200

    Create getUsers and createEvent endpoints

--- sabenco.py
+++ sabenco.py
@@ -16,9 +16,27 @@ def get_db():
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome!"}
 
+# Get the events of a category starting between two dates
 @app.get("/events/{startdate}/{enddate}/{category}", response_model = List[schemas.Event])
 def read_events_by_dates_and_category(startdate: str, enddate: str, category: str, db: Session = Depends(get_db)):
     db_event_by_category = crud.get_event_by_category(db, startdate=startdate, enddate=enddate, category=category)
     return db_event_by_category
+
+# Get the user data given the userId
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+# Create an event given the title, detail, start date and end date
+@app.post("/users/{user_id}/events", response_model = schemas.EventBase)
+def create_event_by_user(event:schemas.EventBase, user_id: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_created_event = crud.create_event(db, event, user_id)
+    return db_created_event
